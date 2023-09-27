5238cbc4ed684ac294eb24bc353e20577638ed3c
Hoda <hhesham2582000@gmail.com>
2023-09-27 22:03:46 +0300

    Add the home layout

---
+++ SportsProject/Module/Home/View/HomeCustomCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCustomCell.swift
+//  SportsProject
+//
+//  Created by Hend on 27/09/2023.
+//
+
+import UIKit
+
+class HomeCustomCell: UICollectionViewCell {
+    @IBOutlet weak var sportName: UILabel!
+    
+    @IBOutlet weak var sportImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        sportImage.layer.cornerRadius = 16
+        sportImage.clipsToBounds = true
+        // Initialization code
+    }
+
+}
